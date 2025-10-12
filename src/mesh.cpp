16f858b577234866aed4afd441da5fca14a1fd29
Dmitry Ponyatov <dponyatov@gmail.com>
2025-10-13 03:11:05 +0400

    .

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -4,16 +4,18 @@ int main(int argc, char *argv[]) {  //
     arg(0, argv[0]);
     for (int i = 1; i < argc; i++) {  //
         arg(i, argv[i]);
-        yyfile = argv[i];
-        assert(yyin = fopen(yyfile, "r"));
-        yyparse();
-        fclose(yyin);
-        yyfile = nullptr;
     }
 }
 
 void arg(int argc, char *argv) {  //
     std::cerr << "argv[" << argc << "] = <" << argv << "]\n";
+    if (argc) {
+        yyfile = argv;
+        assert(yyin = fopen(yyfile, "r"));
+        yyparse();
+        fclose(yyin);
+        yyfile = nullptr;
+    }
 }
 
 void yyerror(const char *msg) {  //
