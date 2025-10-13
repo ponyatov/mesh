1498a09bbd7b0219f97589b41c1745863d8e1162
Dmitry Ponyatov <dponyatov@gmail.com>
2025-10-13 04:02:14 +0400

    .

--- mk/net.mk
+++ mk/net.mk
@@ -10,18 +10,12 @@ PCPP_CFG += -DCMAKE_C_FLAGS="-march=native"
 PCPP_CFG += -DCMAKE_CXX_FLAGS="-march=native"
 
 .PHONY: pcpp
-pcpp: lib/pcpp/setup_dpdk.py lib/pcpp/lib/libPacket++.a
+pcpp: lib/pcpp/setup_dpdk.py
 # rm -rf lib/pcpp tmp/pcpp ; time make pcpp
+lib/pcpp/setup_dpdk.py: tmp/pcpp/setup_dpdk.py
+	sed '1s/python/python3/' $< > $@ ; chmod +x $@
+tmp/pcpp/setup_dpdk.py: lib/pcpp/lib/libPacket++.a
 lib/pcpp/lib/libPacket++.a: ref/PcapPlusPlus/README.md
-	cmake $(PCPP_CFG) -S $(dir $<) -B tmp/$@ --install-prefix=$(LIB)/pcpp
-	cmake --build   tmp/$@
-	cmake --install tmp/$@
-lib/pcpp/setup_dpdk.py:
-
-# lib/pcpp/setup_dpdk.py: tmp/pcpp/setup_dpdk.py
-# 	sed '1s/python/python3/' $< > $@ ; chmod +x $@
-# tmp/pcpp/setup_dpdk.py: lib/pcpp/lib/libPacket++.a
-# lib/pcpp/lib/libPacket++.a: ref/PcapPlusPlus/README.md
-# 	cmake $(PCPP_CFG) -S $(dir $<) -B tmp/pcpp --install-prefix=$(LIB)/pcpp
-# 	cmake --build   tmp/pcpp -j
-# 	cmake --install tmp/pcpp
+	cmake $(PCPP_CFG) -S $(dir $<) -B tmp/pcpp --install-prefix=$(LIB)/pcpp
+	cmake --build   tmp/pcpp -j
+	cmake --install tmp/pcpp
