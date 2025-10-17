7ba0d328a893c72f2a87151f9827504454c1ac9a
Dmitry Ponyatov <dponyatov@gmail.com>
2025-10-17 11:17:34 +0400

    .

--- mk/ref.mk
+++ mk/ref.mk
@@ -1,3 +1,7 @@
 RF += ref/PcapPlusPlus/README.md
 ref/PcapPlusPlus/README.md:
 	$(GITREF) -b v$(PCPP_VER) https://github.com/seladb/PcapPlusPlus.git $(dir $@)
+
+RF += ref/reciever/package.json
+ref/reciever/package.json:
+	$(GITREF) -b master git@gitflic.ru:dponyatov/reciever.git $(dir $@)
